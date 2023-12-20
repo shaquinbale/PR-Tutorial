71522a012080baf8fde96636f0a5f647f9a45f30
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2023-12-20 09:18:21 -0500

    Add program.py and change README.md

---
+++ program.py
@@ -0,0 +1,10 @@
+# i think i messed up in a couple lines, can yall fix it for me?
+
+favoriteAnimal = innnput("What's your favorite animal?")
+
+if favoriteanimal.lower != "cat":
+    print("wow so you hate cats?"
+    print("that's not very nice", bro.)
+
+elsa:
+    qrint("I also love cats!")
